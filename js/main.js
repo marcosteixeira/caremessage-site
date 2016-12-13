f9ed3ff808ef2a7ad40be3377f6d43da9d132cc6
Chris Alexander <calexander@caremessage.org>
2016-12-12 18:15:27 -0700

    Home page styling + content

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@
 ---
 
 /* JS for the CareMessage website */
-document.addEventListener("DOMContentLoaded", function(event) {
-  var feed = new Instafeed({
-    clientId: '18a704f2b64f4d1b81416291bdc3bbf5',
-    accessToken: '1298762098.18a704f.edf9234b44944402be008dc589c8d9f9',
-    get: 'user',
-    userId: '1298762098'
-  });
-  feed.run();
-});
+
+// TODO: Add Instagram integration. See below for an example integration
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   var feed = new Instafeed({
+//     clientId: '18a704f2b64f4d1b81416291bdc3bbf5',
+//     accessToken: '1298762098.18a704f.edf9234b44944402be008dc589c8d9f9',
+//     get: 'user',
+//     userId: '1298762098'
+//   });
+//   feed.run();
+// });
