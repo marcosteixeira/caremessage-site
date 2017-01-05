9729b5585f39d03a3764e9e5204576a362fa25e3
Chris Alexander <calexander@caremessage.org>
2017-01-05 14:55:11 -0700

    Add Instagram slider to Careers page
    - Use Slick.js for the slider
    - Use Instafeed.js to fetch and list latest posts from the API
    - Use our card class with some customizations to display each post

--- js/careers.js
+++ js/careers.js
@@ -1,3 +1,7 @@
+// JS for the Careers page
+
+// Get position list from Greenhouse API. Greenhouse calls this as a callback.
+// Greenhouse passes in a JS object containing a list of positions.
 function careersJobBuilder(data) {
   if (! (data.jobs && data.jobs.length)) {
     return;
@@ -23,3 +27,51 @@ function careersJobBuilder(data) {
       '<a href="' + job.absolute_url + '" target="_blank">Learn More</a></li>');
   }
 }
+
+// Filter function to truncate image captions
+function instafeedCaptionFilter(image) {
+  var MAX_LENGTH = 150;
+
+  if (image.caption && image.caption.text) {
+    if (image.caption.text.length > MAX_LENGTH) {
+      image.shortCaption = image.caption.text.substr(0, MAX_LENGTH).split(" ")
+        .slice(0, -1).join(" ") + '...';
+    } else {
+      image.shortCaption = image.caption.text;
+    }
+  } else {
+    image.shortCaption = '';
+  }
+
+  return true;
+}
+
+// Initialize and configure the slider
+function initSlider() {
+  $('#instafeed').slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+    variableWidth: true
+  });
+}
+
+// Get a list of the latest posts from Instagram and create the slider
+document.addEventListener("DOMContentLoaded", function(event) {
+  var feed = new Instafeed({
+    clientId: '067b8df98acd4576b589dccbfd21e078',
+    accessToken: '4029666356.067b8df.8a1c86b53c7e4dc480f8492e60be6026',
+    get: 'user',
+    userId: '4029666356',
+    resolution: 'low_resolution',
+    limit: 16,
+    filter: instafeedCaptionFilter,
+    after: initSlider,
+    template: '<div class="card">' +
+      '<figure><a href="{{link}}" target="_blank"><img src="{{image}}" /></a>' +
+      '<figcaption>{{model.shortCaption}}</figcaption></figure></div>'
+  });
+
+  feed.run();
+});
